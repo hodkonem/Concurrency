7165d53df0adb8eb139a94c36cb849d4ec413027
Mikhail Latypov <mikhaillat@yandex.ru>
2024-06-13 07:19:42 +0300

    Создал класс MyThread для демонстрации первого способа создания потоков

--- src/main/java/ru/latypov/Main.java
+++ src/main/java/ru/latypov/Main.java
@@ -2,6 +2,9 @@ package ru.latypov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Start");
+        Thread thread = new MyThread();
+        thread.start();
+        System.out.println("\nFinish");
     }
 }
